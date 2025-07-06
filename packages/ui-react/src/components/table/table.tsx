46fbb83f646e51a58fa7f07258e5e3ad3d2fdef3
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-07-06 23:25:47 +0200

    feat: add sorting menu to table

--- packages/ui-react/src/components/table/table.tsx
+++ packages/ui-react/src/components/table/table.tsx
@@ -1,62 +1,23 @@
 import {
-  createColumnHelper,
   flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
+  type Header,
   type Table as TanstackTable,
-  useReactTable,
 } from "@tanstack/react-table";
-import { EllipsisVerticalIcon, ListFilterIcon } from "lucide-react";
-import { type ComponentProps, memo, useMemo, useState } from "react";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ChevronsUpDownIcon,
+  EllipsisVerticalIcon,
+  ListFilterIcon,
+} from "lucide-react";
+import { memo, useMemo } from "react";
+import { Fragment } from "react/jsx-runtime";
 import { cn } from "../../lib/utils.js";
 import { Button } from "../button/button.js";
 import { Card } from "../card/card.js";
+import * as Menu from "../menu/menu.js";
 
-type Attendee = {
-  firstName: string;
-  lastName: string;
-  role: "scout" | "leader" | "volunteer";
-};
-
-const defaultData: Attendee[] = [
-  { firstName: "Alice", lastName: "Smith", role: "scout" },
-  { firstName: "Bob", lastName: "Johnson", role: "leader" },
-  { firstName: "Charlie", lastName: "Brown", role: "volunteer" },
-];
-
-const columnHelper = createColumnHelper<Attendee>();
-
-const columns = [
-  columnHelper.accessor("firstName", {
-    header: () => "Förnamn",
-  }),
-  columnHelper.accessor("lastName", {
-    header: () => "Efternamn",
-  }),
-  columnHelper.accessor("role", {
-    header: () => "Roll",
-    cell: (info) => {
-      const role = info.getValue();
-      return role.charAt(0).toUpperCase() + role.slice(1);
-    },
-  }),
-];
-
-function TableTest() {
-  const [data, _setData] = useState(() => [...defaultData]);
-
-  const table = useReactTable({
-    data,
-    columns,
-    columnResizeMode: "onChange",
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-  });
-
-  return <Table table={table} className="w-full" />;
-}
-
-export type Props<TData> = ComponentProps<"table"> & {
+export type Props<TData> = Parameters<typeof Card>[0] & {
   table: TanstackTable<TData>;
 };
 
@@ -81,115 +42,80 @@ function Table<TData>(props: Props<TData>) {
   }, [table.getState().columnSizingInfo, table.getState().columnSizing]);
 
   return (
-    <>
-      <pre className="mb-4 min-h-48">
-        {JSON.stringify(
-          {
-            // columnSizing: table.getState().columnSizing,
-            // columnSizeVars,
-          },
-          null,
-          2,
-        )}
-      </pre>
-
-      <Card
-        className={cn("p-0 inline-block overflow-auto", className)}
-        {...otherProps}
-        variant="light"
+    <Card
+      className={cn("p-0 inline-block overflow-auto", className)}
+      {...otherProps}
+      variant="light"
+    >
+      <table
+        style={{
+          ...columnSizeVars,
+          width:
+            (table.options.enableColumnResizing ?? true)
+              ? table.getTotalSize()
+              : "100%",
+        }}
       >
-        <table
-          style={{
-            ...columnSizeVars,
-            width:
-              (table.options.enableColumnResizing ?? true)
-                ? table.getTotalSize()
-                : "100%",
-          }}
-        >
-          <thead className="border-b border-gray-300">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    className="relative p-2 text-left"
-                    style={{
-                      width: `calc(var(--header-${header?.id}-size) * 1px)`,
-                    }}
-                  >
-                    <div className="flex justify-between pr-1">
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-
-                      <div className="flex gap-0.5">
-                        <Button size="tiny-icon" variant="text" color="gray">
-                          <ListFilterIcon />
-                        </Button>
-                        <Button size="tiny-icon" variant="text" color="gray">
-                          <EllipsisVerticalIcon />
-                        </Button>
-                      </div>
-                    </div>
-
-                    {header.column.getCanResize() && (
-                      <div
-                        // FIXME: Hiding this from screen readers is not ideal.
-                        // Resizing columns might not be important for fully sight
-                        // impaired users, but it could be for low vision users.
-                        // https://github.com/Scouterna/ui/issues/15
-                        aria-hidden
-                        onDoubleClick={() => header.column.resetSize()}
-                        onMouseDown={header.getResizeHandler()}
-                        onTouchStart={header.getResizeHandler()}
-                        className={cn(
-                          "absolute flex justify-center items-center py-2 top-0 right-0 h-full w-2 cursor-col-resize touch-none select-none",
-                          "after:w-0.5 after:h-full after:bg-gray-300",
-                          header.column.getIsResizing() &&
-                            "bg-blue-100 after:invisible",
-                        )}
-                        style={{
-                          transform:
-                            table.options.columnResizeMode === "onEnd" &&
-                            header.column.getIsResizing()
-                              ? `translateX(${table.getState().columnSizingInfo.deltaOffset}px)`
-                              : "",
-                        }}
-                      ></div>
-                    )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          {table.getState().columnSizingInfo.isResizingColumn ? (
-            <MemoizedTableBody table={table} />
-          ) : (
-            <TableBody table={table} />
-          )}
-          <tfoot>
-            {table.getFooterGroups().map((footerGroup) => (
-              <tr key={footerGroup.id}>
-                {footerGroup.headers.map((header) => (
-                  <th key={header.id}>
+        <thead className="border-b border-gray-300">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="relative p-2 text-left"
+                  style={{
+                    width: `calc(var(--header-${header?.id}-size) * 1px)`,
+                  }}
+                >
+                  <div className="flex justify-between pr-1">
                     {header.isPlaceholder
                       ? null
                       : flexRender(
-                          header.column.columnDef.footer,
+                          header.column.columnDef.header,
                           header.getContext(),
                         )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </tfoot>
-        </table>
-      </Card>
-    </>
+
+                    <div className="flex gap-0.5">
+                      <Button size="tiny-icon" variant="text" color="gray">
+                        <ListFilterIcon />
+                      </Button>
+                      <HeaderMenu header={header} />
+                    </div>
+                  </div>
+
+                  {header.column.getCanResize() && (
+                    <ResizeHandle table={table} header={header} />
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        {table.getState().columnSizingInfo.isResizingColumn ? (
+          <MemoizedTableBody table={table} />
+        ) : (
+          <TableBody table={table} />
+        )}
+
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.footer,
+                        header.getContext(),
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </Card>
   );
 }
 
@@ -209,10 +135,104 @@ function TableBody<TData>({ table }: { table: TanstackTable<TData> }) {
   );
 }
 
-//special memoized wrapper for our table body that we will use during column resizing
+function ResizeHandle({
+  table,
+  header,
+}: {
+  // biome-ignore lint/suspicious/noExplicitAny: We don't care about the type here
+  table: TanstackTable<any>;
+  // biome-ignore lint/suspicious/noExplicitAny: We don't care about the type here
+  header: Header<any, any>;
+}) {
+  return (
+    <div
+      // FIXME: Hiding this from screen readers is not ideal.
+      // Resizing columns might not be important for fully sight
+      // impaired users, but it could be for low vision users.
+      // https://github.com/Scouterna/ui/issues/15
+      aria-hidden
+      onDoubleClick={() => header.column.resetSize()}
+      onMouseDown={header.getResizeHandler()}
+      onTouchStart={header.getResizeHandler()}
+      className={cn(
+        "absolute flex justify-center items-center py-2 top-0 right-0 h-full w-2 cursor-col-resize touch-none select-none",
+        "after:w-0.5 after:h-full after:bg-gray-300",
+        header.column.getIsResizing() && "bg-blue-100 after:invisible",
+      )}
+      style={{
+        transform:
+          table.options.columnResizeMode === "onEnd" &&
+          header.column.getIsResizing()
+            ? `translateX(${table.getState().columnSizingInfo.deltaOffset}px)`
+            : "",
+      }}
+    />
+  );
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: We don't care about the type here
+function HeaderMenu({ header }: { header: Header<any, any> }) {
+  const menuSections = [
+    header.column.getCanSort() && (
+      <Fragment key="sortMenu">
+        {header.column.getIsSorted() !== "asc" && (
+          <Menu.Item
+            icon={<ArrowUpIcon />}
+            onClick={() => header.column.toggleSorting(false)}
+          >
+            Sort Ascending
+          </Menu.Item>
+        )}
+        {header.column.getIsSorted() !== "desc" && (
+          <Menu.Item
+            icon={<ArrowDownIcon />}
+            onClick={() => header.column.toggleSorting(true)}
+          >
+            Sort Descending
+          </Menu.Item>
+        )}
+        {header.column.getIsSorted() && (
+          <Menu.Item
+            icon={<ChevronsUpDownIcon />}
+            onClick={() => header.column.clearSorting()}
+          >
+            Clear Sorting
+          </Menu.Item>
+        )}
+      </Fragment>
+    ),
+  ].filter((section) => section !== false);
+
+  if (menuSections.length === 0) {
+    return null;
+  }
+
+  return (
+    <Menu.Root>
+      <Menu.Trigger
+        render={
+          <Button size="tiny-icon" variant="text" color="gray">
+            <EllipsisVerticalIcon />
+          </Button>
+        }
+      />
+
+      <Menu.Positioner align="start">
+        {menuSections.map((section, index) => (
+          <>
+            {section}
+            {index < menuSections.length - 1 && <Menu.Separator />}
+          </>
+        ))}
+      </Menu.Positioner>
+    </Menu.Root>
+  );
+}
+
+// Special memoized wrapper for our table body that we will use during column resizing
 export const MemoizedTableBody = memo(
   TableBody,
   (prev, next) => prev.table.options.data === next.table.options.data,
 ) as typeof TableBody;
 
-export { Table, TableTest };
+export { Table };
