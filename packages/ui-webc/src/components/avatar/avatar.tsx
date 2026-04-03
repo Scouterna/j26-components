ba60fd3fb99b790992baf0cda2893cdc9b45e715
daBack <daback@daback.dev>
2026-04-03 20:02:18 +0200

    feat: Avatar component (#104)

---
+++ packages/ui-webc/src/components/avatar/avatar.tsx
@@ -0,0 +1,47 @@
+import {
+  Component,
+  type ComponentInterface,
+  getAssetPath,
+  h,
+  Prop,
+} from "@stencil/core";
+
+/**
+ * The avatar component is used to display the user's profile picture.
+ * Wrap it with a container, to determine its size, since it scales to fit container.
+ */
+@Component({
+  tag: "scout-avatar",
+  styleUrl: "avatar.css",
+  shadow: {
+    delegatesFocus: true,
+  },
+  assetsDirs: ["assets"],
+})
+export class ScoutAvatar implements ComponentInterface {
+  /**
+   * The source URL of the user image.
+   */
+  @Prop() imageSrc = "";
+
+  /**
+   * The name of the user.
+   */
+  @Prop() alt = "";
+
+  render() {
+    const getImagePath = () => {
+      if (this.imageSrc) {
+        return this.imageSrc;
+      }
+      try {
+        return getAssetPath("assets/fallbackImage.png");
+      } catch (_e) {
+        console.info("In storybook, rendering from storybook asset");
+        return "fallbackImage.png";
+      }
+    };
+
+    return <img src={getImagePath()} alt={this.alt} />;
+  }
+}
---
+++ packages/ui-webc/src/components/avatar/readme.md
@@ -0,0 +1,21 @@
+# scout-avatar
+
+<!-- Auto Generated Below -->
+
+
+## Overview
+
+The avatar component is used to display the user's profile picture.
+Wrap it with a container, to determine its size, since it scales to fit container.
+
+## Properties
+
+| Property   | Attribute   | Description                       | Type     | Default |
+| ---------- | ----------- | --------------------------------- | -------- | ------- |
+| `alt`      | `alt`       | The name of the user.             | `string` | `""`    |
+| `imageSrc` | `image-src` | The source URL of the user image. | `string` | `""`    |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
